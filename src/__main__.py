c5a79ad50efb101994dae58fcfd10d91d76d3abd
PatriotRossii <patriotrossii2019@mail.ru>
2022-09-28 23:17:48 +0300

    Print info about wind and visibility

--- src/__main__.py
+++ src/__main__.py
@@ -13,9 +13,11 @@ if __name__ == "__main__":
 
 	print("Город:", city)
 	print("Погодные условия:", data["weather"][0]["description"])
-	print("температура:", data["main"]["temp"])
+	print("Температура:", data["main"]["temp"])
 	print("Минимальная температура:", data["main"]["temp_min"])
 	print("Максимальная температура:", data["main"]["temp_max"])
+	print("Видимость (м):", data["visibility"])
+	print("Скорость ветра (м/c):", data["wind"]["speed"])
 
 	res = requests.get("http://api.openweathermap.org/data/2.5/forecast", params={
 		"q": city, "units": "metric", "lang": "ru", "APPID": appid
@@ -24,8 +26,9 @@ if __name__ == "__main__":
 
 	print("Прогноз погоды на неделю")
 	for element in data["list"]:
-		print(
-			"Дата <", element["dt_txt"], ">\r\nТемпература <{0:+3.0f}".format(element["main"]["temp"]),
-			"> \r\nПогодные условия <", element["weather"][0]["description"], ">"
-		)
+		print(f"Дата <{element['dt_txt']}>")
+		print("Температура <{0:+3.0f}>".format(element["main"]["temp"]))
+		print(f"Видимость (м) <{element['visibility']}>")
+		print(f"Скорость ветра (м/с) <{element['wind']['speed']}>")
+		print(f"Погодные условия <{element['weather'][0]['description']}>")
 		print("_____________________________")
