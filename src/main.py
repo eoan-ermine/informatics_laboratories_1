582dc9f284abf1e989e0e60477e1c563e65a75a3
PatriotRossii <patriotrossii2019@mail.ru>
2022-09-28 23:01:33 +0300

    Implement weather and forecast fetch and print

---
+++ src/main.py
@@ -0,0 +1,28 @@
+import requests
+
+city = "Moscow,RU"
+appid = "92175a86b2cd01784df5908960bfd37f"
+
+res = requests.get("http://api.openweathermap.org/data/2.5/weather", params={
+	"q": city, "units": "metric", "lang": "ru", "APPID": appid
+})
+data = res.json()
+
+print("Город:", city)
+print("Погодные условия:", data["weather"][0]["description"])
+print("температура:", data["main"]["temp"])
+print("Минимальная температура:", data["main"]["temp_min"])
+print("Максимальная температура:", data["main"]["temp_max"])
+
+res = requests.get("http://api.openweathermap.org/data/2.5/forecast", params={
+	"q": city, "units": "metric", "lang": "ru", "APPID": appid
+})
+data = res.json()
+
+print("Прогноз погоды на неделю")
+for element in data["list"]:
+	print(
+		"Дата <", element["dt_txt"], ">\r\nТемпература <{0:+3.0f}".format(element["main"]["temp"]),
+		"> \r\nПогодные условия <", element["weather"][0]["description"], ">"
+	)
+	print("_____________________________")
